f59729692b7961d563b8e43b93e4659ef4d801ab
Sergio Hidalgo <barcecheo2319@gmail.com>
2025-04-13 23:59:17 -0500

    Add logic for back button

--- lib/screens/new_league/new_league_teams.dart
+++ lib/screens/new_league/new_league_teams.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_league_flutter/screens/main_screen.dart';
 
 class NewLeagueTeams extends StatelessWidget {
   final String leagueId;
@@ -7,9 +8,46 @@ class NewLeagueTeams extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Detalles de League')),
-      body: Center(child: Text('League ID: $leagueId')),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, Object? result) async {
+        final salir = await _mostrarAlerta(context);
+        if (salir) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MainScreen()),
+          );
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text('Agregar equipos')),
+        body: Center(child: Text('League ID: $leagueId')),
+      ),
     );
+  }
+
+  Future<bool> _mostrarAlerta(BuildContext context) async {
+    return await showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('¿Salir?'),
+              content: Text(
+                'No te preocupes, puedes continuar editando tu torneo después. Sólo buscalo en la sección \'Mi torneo\' desde la pantalla principal.',
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: Text('Sí, salir'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: Text('Seguir editando'),
+                ),
+              ],
+            );
+          },
+        ) ??
+        false;
   }
 }
