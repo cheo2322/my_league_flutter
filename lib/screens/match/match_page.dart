07ecf610c2f1c2ccd07ada61566e7c1b9e8ef5f6
Sergio Hidalgo <barcecheo2319@gmail.com>
2025-09-07 23:15:26 -0500

    US-015: Add loader to header

--- lib/screens/match/match_page.dart
+++ lib/screens/match/match_page.dart
@@ -68,7 +68,7 @@ class _MatchState extends State<Match> {
                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: _buildDynamicContent(context),
+                  children: _buildDynamicContent(context, primaryColor),
                 ),
               ),
             ),
@@ -84,154 +84,167 @@ class _MatchState extends State<Match> {
     );
   }
 
-  List<Widget> _buildDynamicContent(BuildContext context) {
-    return [
-      Row(
-        children: [
-          IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => Navigator.pop(context),
-          ),
-          const SizedBox(width: 16),
-
-          if (widget.match.status == "FINISHED")
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  decoration: BoxDecoration(
-                    color: Colors.white24,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Text(
-                    'FINALIZADO',
-                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
+  List<Widget> _buildDynamicContent(BuildContext context, Color color) {
+    return isLoadingMatchDetails
+        ? [
+          Expanded(
+            child: Container(
+              color: color,
+              child: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                 ),
               ),
             ),
-        ],
-      ),
-
-      Center(
-        child: Container(
-          constraints: BoxConstraints(maxWidth: 250),
-          child: Text(
-            widget.leagueName,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white),
           ),
-        ),
-      ),
+        ]
+        : [
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () => Navigator.pop(context),
+              ),
+              const SizedBox(width: 16),
 
-      const SizedBox(height: 20),
-
-      Row(
-        children: [
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Flexible(
-                  child: Text(
-                    widget.match.homeTeam,
-                    style: isHomeWinner(widget.match) ? winnerStyle : normalStyle,
-                    softWrap: true,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.right,
-                  ),
-                ),
-                const SizedBox(width: 8),
-                const Icon(Icons.sports_soccer, size: 16, color: Colors.white),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  widget.match.date,
-                  style: const TextStyle(
-                    fontSize: 9,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  widget.match.status != "SCHEDULED"
-                      ? '${widget.match.homeResult} - ${widget.match.visitResult}'
-                      : widget.match.time,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                if (widget.match.status != "SCHEDULED")
-                  Text(
-                    widget.match.status == "FINISHED" ? 'FINALIZADO' : 'EN JUEGO',
-                    style: const TextStyle(
-                      fontSize: 9,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+              if (widget.match.status == "FINISHED")
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.white24,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Text(
+                        'FINALIZADO',
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Icon(Icons.sports_soccer, size: 16, color: Colors.white),
-                const SizedBox(width: 8),
-                Flexible(
-                  child: Text(
-                    widget.match.visitTeam,
-                    style: isVisitWinner(widget.match) ? winnerStyle : normalStyle,
-                    softWrap: true,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
                 ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      const SizedBox(height: 6),
-
-      if (matchDetails != null && matchDetails!.field != null)
-        Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.location_on, size: 16, color: Colors.white70),
-              SizedBox(width: 4),
-              Text(matchDetails!.field!, style: TextStyle(color: Colors.white70)),
             ],
           ),
-        ),
 
-      const Spacer(),
+          Center(
+            child: Container(
+              constraints: BoxConstraints(maxWidth: 250),
+              child: Text(
+                widget.leagueName,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
 
-      // Tabs
-      TabBar(
-        tabs: tabs,
-        indicatorColor: Colors.white,
-        labelColor: Colors.white,
-        labelPadding: EdgeInsets.symmetric(horizontal: 5),
-        labelStyle: TextStyle(fontWeight: FontWeight.bold),
-        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
-        unselectedLabelColor: Colors.white,
-      ),
-    ];
+          const SizedBox(height: 20),
+
+          Row(
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        widget.match.homeTeam,
+                        style: isHomeWinner(widget.match) ? winnerStyle : normalStyle,
+                        softWrap: true,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Icon(Icons.sports_soccer, size: 16, color: Colors.white),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.match.date,
+                      style: const TextStyle(
+                        fontSize: 9,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      widget.match.status != "SCHEDULED"
+                          ? '${widget.match.homeResult} - ${widget.match.visitResult}'
+                          : widget.match.time,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    if (widget.match.status != "SCHEDULED")
+                      Text(
+                        widget.match.status == "FINISHED" ? 'FINALIZADO' : 'EN JUEGO',
+                        style: const TextStyle(
+                          fontSize: 9,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Icon(Icons.sports_soccer, size: 16, color: Colors.white),
+                    const SizedBox(width: 8),
+                    Flexible(
+                      child: Text(
+                        widget.match.visitTeam,
+                        style: isVisitWinner(widget.match) ? winnerStyle : normalStyle,
+                        softWrap: true,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 6),
+
+          if (matchDetails != null && matchDetails!.field != null)
+            Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.location_on, size: 16, color: Colors.white70),
+                  SizedBox(width: 4),
+                  Text(matchDetails!.field!, style: TextStyle(color: Colors.white70)),
+                ],
+              ),
+            ),
+
+          const Spacer(),
+
+          // Tabs
+          TabBar(
+            tabs: tabs,
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            labelPadding: EdgeInsets.symmetric(horizontal: 5),
+            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+            unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+            unselectedLabelColor: Colors.white,
+          ),
+        ];
   }
 
   bool isHomeWinner(MatchDto match) =>
